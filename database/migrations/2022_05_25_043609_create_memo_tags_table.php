9c9e643bd396ce2e14aa7f00169dc3835de6dbac
kodaishiotsuki <0711dr5@gmail.com>
2022-05-25 15:36:11 +0900

    first commit

---
+++ database/migrations/2022_05_25_043609_create_memo_tags_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMemoTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('memo_tags', function (Blueprint $table) {
+            //中間テーブル
+            $table->unsignedBigInteger('memo_id');
+            $table->unsignedBigInteger('tag_id');
+
+            //外部キー制約
+            $table->foreign('memo_id')->references('id')->on('memos');
+            $table->foreign('tag_id')->references('id')->on('tags');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('memo_tags');
+    }
+}
